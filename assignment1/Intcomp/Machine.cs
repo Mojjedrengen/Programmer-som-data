33fa3d54bac310d4f2a78d12249f8c8313d9476e
Marius Larsen <54457839+MessiGames30@users.noreply.github.com>
2026-08-30 16:11:12 +0200

    Added assignment 1.4 d Open for input on sub simplification.

--- assignment1/Intcomp/Machine.cs
+++ assignment1/Intcomp/Machine.cs
@@ -5,6 +5,7 @@ abstract class Expr
 {
     public abstract override string ToString();
     public abstract int Eval(Dictionary<string, int> env);
+    public abstract Expr Simplify();
 }
 
 class CstI : Expr
@@ -25,7 +26,11 @@ class CstI : Expr
     {
         return Value;
     }
-    
+
+    public override Expr Simplify()
+    {
+        return this;
+    }
 }
 
 class Var : Expr
@@ -50,6 +55,11 @@ class Var : Expr
         }
 
         throw new Exception("Variable does not exist in env");
+    }
+    
+    public override Expr Simplify()
+    {
+        return this;
     }
 }
 
@@ -79,6 +89,22 @@ class Add : Binop
         return (E1.Eval(env) + E2.Eval(env));
     }
     
+    public override Expr Simplify()
+    {
+        Expr s1 = E1.Simplify();
+        Expr s2 = E2.Simplify();
+        
+        switch (s1,s2)
+        {
+            case (CstI{Value:0},var to):
+                return to;
+            case (var en, CstI{Value:0}):
+                return en;
+            default:
+                return new Add(s1, s2);
+        }
+    }
+    
 }
 
 class Mul : Binop
@@ -93,6 +119,26 @@ class Mul : Binop
     public override int Eval(Dictionary<string, int> env)
     {
         return (E1.Eval(env) * E2.Eval(env));
+    }
+    
+    public override Expr Simplify()
+    {
+        Expr s1 = E1.Simplify();
+        Expr s2 = E2.Simplify();
+        
+        switch (s1,s2)
+        {
+            case (CstI{Value:0},var to):
+                return new CstI(0);
+            case (var en, CstI{Value:0}):
+                return new CstI(0);
+            case (CstI{Value:1},var to):
+                return to;
+            case (var en, CstI{Value:1}):
+                return en;
+            default:
+                return new Mul(s1, s2);
+        }
     }
 }
 
@@ -109,10 +155,28 @@ class Sub : Binop
     {
         return (E1.Eval(env) - E2.Eval(env));
     }
+    
+    public override Expr Simplify()
+    {
+        Expr s1 = E1.Simplify();
+        Expr s2 = E2.Simplify();
+        
+        // Havent found a good way to compare the two in e1 = e2 case returning 0
+        // Couldnt fint a good comparer for classes. 
+        switch (s1,s2)
+        {
+            case (var en, CstI{Value:0}):
+                return en;
+            case (var en, var to) when en.ToString() == to.ToString():
+                return (new CstI(0));
+            default:
+                return new Sub(s1, s2);
+        }
+    }
 }
 
 
-class Machine
+class Program
 {
     static void Main()
     {
@@ -121,8 +185,12 @@ class Machine
         Expr e1 = new Mul(new CstI(1), new Var("en"));
         Expr e2 = new Sub(new CstI(2), new Var("to"));
         Expr e3 = new Add(new CstI(3), new Var("tre"));
+        Expr e4 = new Sub(new CstI(2), new CstI(2));
+        Expr e5 = new Sub(new CstI(2), new CstI(0));
+        Expr e6 = e4.Simplify();
+        Expr e7 = e5.Simplify();
 
-        List<Expr> explist = new List<Expr> { e, e1, e2, e3 };
+        List<Expr> explist = new List<Expr> { e, e1, e2, e3, e4, e5,e6,e7};
 
         foreach (Expr xp in explist)
         {
